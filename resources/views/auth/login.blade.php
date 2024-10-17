ac82d4ba503b54fee00adfea12b77172f3001e73
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-17 21:57:52 +0900

    fix kernel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
-        {{dd("hello login")}}
+       
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
@@ -44,5 +44,6 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        {{dd("hello login")}}
     </form>
 </x-guest-layout>
