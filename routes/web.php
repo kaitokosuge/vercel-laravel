ab4b8404dc65c91112ba93e7c7176d320e136933
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-17 21:35:31 +0900

    fix kernel

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get("/posts",function () {
 });
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
