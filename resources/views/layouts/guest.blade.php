4bf9691b1c26a579951c089e2cd8935c4938ef9b
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-17 22:05:34 +0900

    fix kernel

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,10 +21,11 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
-{{dd("hello guest")}}
+
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
+        {{dd("hello guest")}}
     </body>
 </html>
