42a6f0a157d90d859850b6a1e1c7306836b461f2
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-17 21:31:52 +0900

    fix kernel

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get("/posts",function () {
+    return view('next');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
