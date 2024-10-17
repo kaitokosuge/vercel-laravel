83dedff552e23719960fd499fbc715a1643dede1
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-18 00:27:20 +0900

    fix kernel

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,6 +36,7 @@ class RegisteredUserController extends Controller
         //     'password' => ['required', 'confirmed', Rules\Password::defaults()],
         // ]);
         // dd($validateData);
+        dd("helllo");
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
