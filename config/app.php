cf609b7d89f2927118960aceee101f4e677c6a12
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-18 00:02:59 +0900

    fix kernel

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://vercel-laravel-4yfalrprz-kaitokosuges-projects.vercel.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
