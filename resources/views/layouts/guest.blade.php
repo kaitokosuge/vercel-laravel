4b372d16e4555bf71ad68c0165cf7b58b12df814
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-10-20 19:52:25 +0900

    fix db

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-BhOUt4Pv.css') }}">
         <script src="{{ asset('build/assets/app-BjCBnTiP.js') }}" defer></script> --}}
     </head>
